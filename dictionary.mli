d31740937a05ffc94472a6cda070785b93c11446
Itai <il255@cornell.edu>
2019-04-16 10:29:46 -0400

    Fixed dictionary.mli

--- dictionary.mli
+++ dictionary.mli
@@ -5,11 +5,16 @@
 (**********************************************************************)
 
 (** [Dict] is the module used to maintain a Scrabble dictionary *)
-module Dict: Set.S
+module Dict: Set.S with type elt = string
+
 
 (** [create_dictionary] is a dictionary containing all words in the official
   * Scrabble dictionary*)
 val create_dictionary: Dict.t
+
+(** [is_member s dict] tests whether or not input string [s] is a member of 
+  * dictionary [dict] *)
+val is_member: Dict.t -> string -> bool
 
 
 
